878f7aac4f0db8e6f463751b8e2894d823ed2682
Sergii Solianyk <ssolianyk@szkla.pl>
2016-11-17 09:49:43 +0100

    Migrations implementation started

---
+++ src/Szkla/Bundle/ProductGridBundle/Migrations/Schema/v1_0/SzklaProductGridBundle.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Szkla\Bundle\ProductGridBundle\Migrations\Schema\v1_0;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+/**
+ * Migration Class SzklaProductGridBundle
+ *
+ */
+class SzklaProductGridBundle implements Migration
+{
+
+    /**
+     * @inheritdoc
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        // @codingStandardsIgnoreStart
+
+        // @codingStandardsIgnoreEnd
+    }
+}
